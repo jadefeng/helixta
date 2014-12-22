275c88c567ce7b3fb70cfb7304a968dbd05f009a
jadefeng <jade.jiefeng@gmail.com>
2014-12-22 18:25:39 +1100

    finished exercises

--- brainteasers/linkedLists.rb
+++ brainteasers/linkedLists.rb
@@ -1,4 +1,4 @@
-require 'pry'
+# require 'pry'
 # Find if there is a loop in a linked list
 
 # Creating a linked list
@@ -6,48 +6,38 @@ class LinkedList
 	attr_accessor :val, :next
 
 	def initialize(val_, next_)
-		@val_ = val_
-		@next_ = next_
-	end
-
-	def next 
-		# Method to reference the 'next' method
-	end
-
-	def val
-		# Method to reference value
+		@val = val_
+		@next = next_
 	end
 
 end
 
-ll4 = LinkedList.new(3, nil)
+ll4 = LinkedList.new(4, nil)
 ll3 = LinkedList.new(3, ll4)
 ll2 = LinkedList.new(2, ll3)
 ll1 = LinkedList.new(1, ll2)
 
 # Testing for loops
 
-## TODO - find out how to access .next and .val attributes in a linkedList
 def findLoop(llist)
 	linked_list_map = {}
 	linked_list = llist
 
-	if linked_list.next == nil
-		puts "There is no loop in this linked list"
-	else
-		if linked_list_map[linked_list] == true
-			# search through linked_list_map that linked_list exists as a key
-			puts "There is a loop in this linked list"
-		else	# If it does not exist
-			linked_list_map[linked_list] = true 	# Add the linkedlist to the map, with the key being the linkedlist and the value being true
-			linked_list = linked_list.next 			# Updated the linked_list value
+	until linked_list.next == nil
+		if linked_list_map[linked_list] == true 		# search through linked_list_map that linked_list exists as a key
+			return true 								# We found a loop! 
+		else											# If there is no loop in this node of a linked list (yet)
+			linked_list_map[linked_list] = true 		# Add the linkedlist to the map, with the key being the linkedlist and the value being true
+			linked_list = linked_list.next 				# Updated the linked_list value for the next iteration
 		end
 	end
+	return false 										# There is no loop in this entire linked list!
 end
 
-findLoop(ll1)
+puts findLoop(ll1)  	# => false 
 
-ll4 = LinkedList.new(3, ll1)
-findLoop(ll1)
+puts "Creating a loop"
+ll4.next = ll1
+puts findLoop(ll1)		# => true
 
-binding.pry
+# binding.pry
